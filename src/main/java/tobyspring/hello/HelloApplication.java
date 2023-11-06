a90ae617377190612c78864d0dce385594e35939
sun2898 <alfo3347@cnu.ac.kr>
2023-11-06 20:37:51 +0900

    Servelet Container 생성, 등록

--- src/main/java/tobyspring/hello/HelloApplication.java
+++ src/main/java/tobyspring/hello/HelloApplication.java
@@ -3,46 +3,23 @@ package tobyspring.hello;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class HelloApplication {
 	public static void main(String[] args) {
-		GenericApplicationContext applicationContext = new GenericApplicationContext();
+		//Spring Container 생성
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
 		applicationContext.registerBean(HelloController.class); //object를 넘기는 것이 아니고 class 정보만 넘긴다
 		applicationContext.registerBean(SimpleHelloService.class);
 		applicationContext.refresh();
 
+		//Servlet Container를 코드로 생성 + 등록
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			servletContext.addServlet("frontcontroller", new HttpServlet() {
-				@Override
-				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-					//인증, 보안, 다국어처리 등 공통 기능
-					if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-						String name = req.getParameter("name");
-
-						HelloController helloController = applicationContext.getBean(HelloController.class);
-						String ret = helloController.hello(name);
-
-						//응답의 3요소 : 상태코드(status), 헤더(header), 바디(body)
-						//resp.setStatus(HttpStatus.OK.value()); 생략가능 기본적으로 200코드로 던져줌
-						resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-						resp.getWriter().println(ret);
-					} else {
-						resp.setStatus(HttpStatus.NOT_FOUND.value());
-					}
-
-				}
-			}).addMapping("/*");
+			servletContext.addServlet("dispatcherServlet",
+						new DispatcherServlet(applicationContext)
+					).addMapping("/*");
 		});
 		webServer.start();
 	}
--- src/main/java/tobyspring/hello/HelloController.java
+++ src/main/java/tobyspring/hello/HelloController.java
@@ -1,7 +1,11 @@
 package tobyspring.hello;
 
-import java.util.Objects;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Objects;
+@RequestMapping("/hello")
 public class HelloController {
 
     private final HelloService helloService;
@@ -10,7 +14,8 @@ public class HelloController {
         this.helloService = helloService;
     }
 
-
+    @GetMapping
+    @ResponseBody
     public String hello(String name) {
 
         return helloService.sayHello(Objects.requireNonNull(name)); //name값이 null인것을 방지
