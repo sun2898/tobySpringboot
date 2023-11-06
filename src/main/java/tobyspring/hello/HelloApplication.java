74d90c7ad9949bea3c4fe7217e54580b6e5d70a6
sun2898 <alfo3347@cnu.ac.kr>
2023-11-06 21:29:45 +0900

    run()

--- src/main/java/tobyspring/hello/HelloApplication.java
+++ src/main/java/tobyspring/hello/HelloApplication.java
@@ -1,48 +1,28 @@
 package tobyspring.hello;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 //Configuration 어노테이션이 붙은 class 는 맨처음 container 에 등록된다
 @Configuration
+@ComponentScan // Component 가 붙은 class 들을 찾아서 Bean 으로 등록을 해라!
 public class HelloApplication {
 
 	@Bean
-	public HelloController helloController(HelloService helloService) {
-		return new HelloController(helloService);
+	public ServletWebServerFactory servletWebServerFactory() {
+		return new TomcatServletWebServerFactory();
 	}
 
 	@Bean
-	public HelloService helloService() {
-		return new SimpleHelloService();
+	public DispatcherServlet dispatcherServlet() {
+		return new DispatcherServlet();
 	}
 
 	public static void main(String[] args) {
-		//Spring Container 생성
-		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
-			@Override
-			protected void onRefresh() {
-				super.onRefresh();
-
-				//Servlet Container 코드로 생성 + 등록
-				ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-				WebServer webServer = serverFactory.getWebServer(servletContext -> {
-					servletContext.addServlet("dispatcherServlet",
-							new DispatcherServlet(this)
-					).addMapping("/*");
-				});
-				webServer.start();
-			}
-		};
-		applicationContext.register(HelloApplication.class); //object를 넘기는 것이 아니고 class 정보만 넘긴다
-		applicationContext.refresh();
-
-
+		MySpringApplication.run(HelloApplication.class, args);
 	}
-
 }
--- src/main/java/tobyspring/hello/HelloController.java
+++ src/main/java/tobyspring/hello/HelloController.java
@@ -1,25 +1,25 @@
 package tobyspring.hello;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
-@RequestMapping("/hello")
+
+@RestController
 public class HelloController {
 
     private final HelloService helloService;
+    private final ApplicationContext applicationContext;
 
-    public HelloController(HelloService helloService) {
+    public HelloController(HelloService helloService, ApplicationContext applicationContext) {
         this.helloService = helloService;
+        this.applicationContext = applicationContext;
     }
 
-    @GetMapping
-    @ResponseBody
+    @GetMapping("/hello")
     public String hello(String name) {
 
         return helloService.sayHello(Objects.requireNonNull(name)); //name값이 null인것을 방지
     }
-
-
 }
